85636c4704b98574bacf436404d4fbd19af936e7
LILAC\marie <miura.a@marietta.co.jp>
2022-09-25 17:21:12 +0900

    [Add] lesson.py

---
+++ lesson.py
@@ -0,0 +1,37 @@
+# import math
+# num = 1
+# name = "1"
+# new_name = int(name)
+# print(new_name, type(new_name))
+
+# print("hi","mike",sep=",",end=".\n")
+# print("hi","mike",sep=",",end="\n")
+
+# result = math.sqrt(25)
+# print(result)
+
+# print('I don\'t know')
+# print(r'C:\name\name')
+# print("""\
+# line1
+# line2\
+# """)
+# word = "python"
+# print(word[:2])
+# word = "J" + word[1:]
+# print(word)
+# print(len(word))
+
+# 文字列のメソッド
+s = 'My name is Mike. Hi Mike'
+print(s)
+is_start = s.startswith('My')
+print(is_start)
+print(s.find("Mike"))
+print(s.rfind("Mike"))
+print(s.count("Mike"))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.lower())
+print(s.replace("Mike","Nancy"))
